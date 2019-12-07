2cc6cd82f2bbe639037671922374dafc78a3df9b
Zihan Li <lizihan@umich.edu>
2019-12-07 15:42:20 -0500

    yo

--- Hello.cpp
+++ Hello.cpp
@@ -13,7 +13,11 @@
 //===----------------------------------------------------------------------===//
 
 #include "llvm/ADT/Statistic.h"
+#include "llvm/IR/CFG.h"
 #include "llvm/IR/Function.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/IntrinsicInst.h"
+// #include "llvm/IR/LLVMContext.h"
 #include "llvm/Pass.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Analysis/AliasAnalysis.h"
@@ -22,17 +26,24 @@
 #include "llvm/Analysis/LoopInfo.h"
 #include "llvm/Analysis/LoopIterator.h"
 #include "llvm/Analysis/LoopPass.h"
+// #include "llvm/Analysis/MemorySSA.h"
+// #include "llvm/Analysis/MemorySSAUpdater.h"
 #include "llvm/Transforms/Scalar/LoopPassManager.h"
 #include "llvm/Transforms/Utils/LoopUtils.h"
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
 #include <algorithm>
 #include <utility>
 #include "llvm/Analysis/BranchProbabilityInfo.h"
 #include "llvm/Analysis/BlockFrequencyInfo.h"
 #include "llvm/Analysis/DependenceAnalysis.h"
+#include "llvm/IR/CFG.h"
 using namespace llvm;
 
 #define DEBUG_TYPE "hello"
 
+// static LLVMContext TheContext;
+// static IRBuilder<> Builder(TheContext);
+// static std::unique_ptr<Module> TheModule;
 
 namespace {
   // Hello - The first implementation, without getAnalysisUsage.
@@ -46,31 +57,58 @@ namespace {
       DependenceInfo *DI = &getAnalysis<DependenceAnalysisWrapperPass>().getDI();
       LoopInfo *LI = &getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
 
-      errs() << "### Hello: \n";
+      DominatorTree *DT = &getAnalysis<DominatorTreeWrapperPass>().getDomTree();
+      // MemorySSA *MSSA = &getAnalysis<MemorySSAWrapperPass>().getMSSA();
+      // std::unique_ptr<MemorySSAUpdater> MSSAU = make_unique<MemorySSAUpdater>(MSSA);
 
-      for (auto BB1 = L->block_begin(); BB1 != L->block_end(); ++BB1) {
-        for (auto II = (*BB1)->begin(), IE = (*BB1)->end(); II != IE; II++) {
-           Instruction &Dst = *II;
-           for (auto BB2 = L->block_begin(); BB2 != L->block_end(); ++BB2) {
-              for (auto JI = (*BB2)->begin(), JE = (*BB2)->end(); JI != JE; JI++) {
-                 Instruction &Src = *JI;
-                 Dependence *dep;
-                 auto infoPtr = DI->depends(&Src, &Dst, false); /* depends(&Src, &Dest, true); */
-                 dep = infoPtr.get();
-                 if (dep != NULL) {
-                   if (dep->isConfused()) errs() << "[C] ";
-                   dep->getDst()->print(errs(), false);
-                   errs() << "   ---> ";
-                   dep->getSrc()->print(errs(), false);
-                   errs() << "\n";
-                 }
-              }
-           }
-        }
+      auto exitBlock = L->getUniqueExitBlock();
+      if (exitBlock == nullptr) {
+        return false;
       }
 
+      auto exitingBlock = *pred_begin(exitBlock);
+      errs() << "jfdkslajdlksajflkdsa\n";
+
+      auto exitingBranchInst = exitingBlock->end();
+
+      exitingBranchInst--;
+      exitingBranchInst--;
+      exitingBranchInst--;
+
+      Instruction *I = &(*exitingBranchInst);
+      I->print(errs());
+      auto module = I->getFunction()->getParent();
+
+      auto newExitingBlock = SplitBlock(exitingBlock, I, DT, LI);
+      auto threadBlock = SplitBlock(exitingBlock, &(*exitingBlock->begin()), DT, LI);
+      auto detachBlock = exitingBlock;
+
+      exitBlock = L->getUniqueExitBlock();
+      if (exitBlock == nullptr) {
+        return false;
+      }
+      auto newExitBlock = SplitBlock(exitBlock, &(*exitBlock->begin()), DT, LI);
+      
+      auto preHeader = L->getLoopPreheader();
+      IRBuilder<> TmpB(preHeader, preHeader->begin());
+      auto SyncRegion = TmpB.CreateCall(
+        Intrinsic::getDeclaration(module, Intrinsic::syncregion_start), {});
+
+      IRBuilder<> Builder(detachBlock->getTerminator());
+      // Create the detach.
+      DetachInst *DetachI = Builder.CreateDetach(threadBlock, newExitingBlock, dyn_cast<Instruction>(SyncRegion));
+      detachBlock->getTerminator()->eraseFromParent();
+      // Create the reattach.
+      Builder.SetInsertPoint(threadBlock->getTerminator());
+      ReattachInst *ReattachI = Builder.CreateReattach(newExitingBlock, SyncRegion);
+      threadBlock->getTerminator()->eraseFromParent();
+
+      Builder.SetInsertPoint(exitBlock->getTerminator());
+      SyncInst *SyncI = Builder.CreateSync(newExitBlock, SyncRegion);
+      exitBlock->getTerminator()->eraseFromParent();
+
       errs() << "### end\n";
-      return false;
+      return true;
     }
 
     void getAnalysisUsage(AnalysisUsage &AU) const override {
@@ -78,6 +116,10 @@ namespace {
       AU.addRequired<BlockFrequencyInfoWrapperPass>();
       AU.addPreserved<LoopInfoWrapperPass>();
       AU.addRequired<DependenceAnalysisWrapperPass>();
+      AU.addPreserved<DominatorTreeWrapperPass>();
+
+      // AU.addRequired<MemorySSAWrapperPass>();
+      // AU.addPreserved<MemorySSAWrapperPass>();
       getLoopAnalysisUsage(AU);
     }
   };
