7d3128c56cbab0ae327373feddb4e85ba81fdee6
Zihan Li <lizihan@umich.edu>
2019-12-12 17:54:49 -0500

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -94,229 +94,77 @@ namespace {
       BlockFrequencyInfo *BFI = &getAnalysis<BlockFrequencyInfoWrapperPass>().getBFI();
       DependenceInfo *DI = &getAnalysis<DependenceAnalysisWrapperPass>().getDI();
       LoopInfo *LI = &getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
-
       DominatorTree *DT = &getAnalysis<DominatorTreeWrapperPass>().getDomTree();
+
       // MemorySSA *MSSA = &getAnalysis<MemorySSAWrapperPass>().getMSSA();
       // std::unique_ptr<MemorySSAUpdater> MSSAU = make_unique<MemorySSAUpdater>(MSSA);
       // if (L->getParent()->getName() != "main")
       //   return false;
 
-      BasicBlock* preHeader = L->getLoopPreheader();
-      BasicBlock* pfor_detach = preHeader->getUniqueSuccessor();
-      DetachInst* detachInst = dyn_cast<DetachInst>(pfor_detach->getTerminator());
+      errs() << "================================" << "\n";
+      errs() << "===      start our pass      ===" << "\n";
+      errs() << "================================" << "\n";
+
+      BasicBlock* loopHeader = L->getLoopPreheader()->getUniqueSuccessor();
+      DetachInst* detachInst = dyn_cast<DetachInst>(loopHeader->getTerminator());
       if (detachInst == nullptr) return false;
-      BasicBlock* pfor_body = detachInst->getDetached();
-      BasicBlock* pfor_inc = detachInst->getContinue();
+      BasicBlock* loopIfBlock = detachInst->getDetached();
+      BasicBlock* loopLatch = detachInst->getContinue();
+      BranchInst* ifInst = dyn_cast<BranchInst>(loopIfBlock->getTerminator());
 
-      BranchInst* ifInst = dyn_cast<BranchInst>(pfor_body->getTerminator());
-
+      // get infreqBlock and freqBlock
       BasicBlock* infreqBlock;
       BasicBlock* freqBlock;
-
       if (!checkDepAndSetInFB(ifInst, BPI, DI, infreqBlock, freqBlock)) {
         errs() << "infre" << "\n";
         return false;
-
       }
-      errs() << "infre!!!" << "\n";
       errs() << "infreqBlockName " << infreqBlock->getName() << "\n";
       errs() << "freqBlockName " << freqBlock->getName() << "\n";
 
       Function* F = ifInst->getFunction();
-      BasicBlock* if_end = freqBlock->getUniqueSuccessor();
-      ReattachInst* reattachInst = dyn_cast<ReattachInst>(if_end->getTerminator());
+      BasicBlock* loopReattachBlock = freqBlock->getUniqueSuccessor();
 
-      BasicBlock* pfor_body_clone = SplitBlock(pfor_inc, &(*pfor_inc->begin()), DT, LI);
-      std::swap(pfor_body_clone, pfor_inc);
-      errs() << "Print clone instructions: \n";
-
-      for (auto II = pfor_body->begin(), IE = pfor_body->end(); II != IE; ++II) {
-        Instruction* II_clone = (*II).clone();
-        II_clone->insertBefore(pfor_body_clone->getTerminator());
-        II_clone->print(errs());
-        errs() << "\n";
-      }
-      errs() << "Finish clone instructions: \n";
-      pfor_body_clone->getTerminator()->eraseFromParent();
-
-      BranchInst* ifInstClone = dyn_cast<BranchInst>(pfor_body_clone->getTerminator());
-      ICmpInst* ifConditionClone = dyn_cast<ICmpInst>( --( --(pfor_body_clone->end())) );
-      ifInstClone->setSuccessor(0, infreqBlock);
-      ifInstClone->setSuccessor(1, pfor_inc);
-      ifInstClone->setCondition(ifConditionClone);
-      ifInstClone->print(errs());
-      errs() << "\n";
-
+      // clone if block
       ValueToValueMapTy VMap;
-      BasicBlock *pfor_body_temp = CloneBasicBlock(pfor_body, VMap, ".clone", F);
-      SmallVector<BasicBlock *, 1> blockList = {pfor_body_temp};
+      BasicBlock *ifBlockClone = CloneBasicBlock(loopIfBlock, VMap, ".clone", F);
+      SmallVector<BasicBlock *, 1> blockList = {ifBlockClone};
       remapInstructionsInBlocks(blockList, VMap);
-      errs() << "Print clone instructions: \n";
-      for (auto II = pfor_body_temp->begin(), IE = pfor_body_temp->end(); II != IE; ++II) {
-        II->print(errs());
-        errs() << "\n";
-      }
-      errs() << "Finish clone instructions: \n";
 
-      
+      // get cloned if instruction and infrequent branch instruction
+      BranchInst* ifInstClone = dyn_cast<BranchInst>(ifBlockClone->getTerminator());
+      BranchInst* infreqBr = dyn_cast<BranchInst>(infreqBlock->getTerminator());
+      ReattachInst* reattachInst = dyn_cast<ReattachInst>(loopReattachBlock->getTerminator());
 
-      // BasicBlock* if_end_split = SplitBlock(if_end, &(*if_end->getTerminator()), DT, LI);
+      // change true branch of ifBlock and false branch of ifBlockClone
+      ifInst->setSuccessor(0, loopReattachBlock);
+      ifInstClone->setSuccessor(1, loopLatch);
 
-      // Todo, check which one is the frequent block.
-      // PHINode* phi_node = dyn_cast<PHINode>(if_end->begin());
-      // phi_node->print(errs());
-      // BasicBlock* incommingBlock = phi_node->getIncomingBlock(0);
-      // Value* frequentValue;
-      // if (incommingBlock == freqBlock) {
-      //   frequentValue = phi_node->getIncomingValue(0);
-      // } else {
-      //   frequentValue = phi_node->getIncomingValue(1);
-      // }
+      // change infreqBlock to point to latch
+      infreqBr->setSuccessor(0, loopLatch);
 
-      // TODO: detect all usage
-      // StoreInst* store_inst = dyn_cast<StoreInst>(--(--if_end->end()));
-      // store_inst->setOperand(0, frequentValue);
+      // change reattach and continue to cloned block
+      detachInst->setSuccessor(1, ifBlockClone);
+      reattachInst->setSuccessor(0, ifBlockClone);
 
-      // if_end->begin()->eraseFromParent();
+      // add cloned BB to loop
+      L->addBasicBlockToLoop(ifBlockClone, *LI);
 
-      BranchInst* ifThenBr = dyn_cast<BranchInst>(infreqBlock->getTerminator());
-      ifThenBr->setSuccessor(0, pfor_inc);
-
-      BranchInst* infreqBr = dyn_cast<BranchInst>(pfor_body->getTerminator());
-      infreqBr->setSuccessor(0, if_end);
-
-      // magic seal
-      // ifInst->setSuccessor(0, freqBlock);
-
-
-
+      // create sync before infrequent path
       BasicBlock* newSyncBlock = SplitBlock(infreqBlock, &(*infreqBlock->begin()), DT, LI);
       std::swap(newSyncBlock, infreqBlock);
 
-      auto exitBlock = L->getUniqueExitBlock();
+      BasicBlock* exitBlock = L->getUniqueExitBlock();
 
-      Instruction* II_clone = (*exitBlock->begin()).clone();
-      II_clone->insertBefore(newSyncBlock->getTerminator());
-
-      SyncInst* syncI = dyn_cast<SyncInst>(II_clone);
-      syncI->setSuccessor(0, infreqBlock);
-
+      Instruction* syncInstClone = (*exitBlock->begin()).clone();
+      syncInstClone->insertBefore(newSyncBlock->getTerminator());
+      syncInstClone->setSuccessor(0, infreqBlock);
       newSyncBlock->getTerminator()->eraseFromParent();
-
-
-
-
-
-      // BasicBlock* dummy_block = SplitBlock(if_end_split, &(*if_end_split->getTerminator()), DT, LI);
-      // std::swap(dummy_block, if_end_split);
-
-      // BranchInst* ifEndBr = dyn_cast<BranchInst>(if_end->getTerminator());
-      // ifEndBr->setSuccessor(0, if_end_split);
-
-      // for (auto II = infreqBlock->begin(), IE = (--infreqBlock->end()); II != IE; ++II) {
-      //   Instruction* II_clone = (*II).clone();
-      //   II_clone->insertBefore(dummy_block->getTerminator());
-      //   II_clone->print(errs());
-      // }
-
-
-
-
-
-      // pfor_body_clone->getTerminator()->eraseFromParent();
-
-      // BranchInst* ifInstClone = dyn_cast<BranchInst>(pfor_body_clone->getTerminator());
-      // ifInstClone->setSuccessor(1, pfor_inc);
-
-      // detachInst->setSuccessor(1, pfor_body_clone);
-      // reattachInst->setSuccessor(0, pfor_body_clone);
-      // //
-      // //
-      // ifInst->setSuccessor(0, if_end);
-      // //
-      // BranchInst* infreqBr = dyn_cast<BranchInst>(infreqBlock->getTerminator());
-      // infreqBr->setSuccessor(0, pfor_inc);
-      // //
-      // ifInstClone->setSuccessor(0, infreqBlock);
-      //
-
-      // (--(--if_end->end()))->eraseFromParent();
-      // (--(--if_end->end()))->eraseFromParent();
-      // (--(--if_end->end()))->eraseFromParent();
-
-
-
 
       errs() << "### log end\n";
 
       return true;
 
-      // auto exitBlock = L->getUniqueExitBlock();
-      // if (exitBlock == nullptr) {
-      //   return false;
-      // }
-      // // errs() << exitBlock->getParent()->getName() << "\n";
-      // // if (exitBlock->getParent()->getName() != "main") {
-      // //   return false;
-      // // };
-      // // errs() << exitBlock->getParent()->getName() << "\n";
-
-
-      // auto exitingBlock = *pred_begin(exitBlock);
-      // errs() << "jfdkslajdlksajflkdsa\n";
-
-      // auto exitingBranchInst = exitingBlock->end();
-
-      // exitingBranchInst--;
-      // exitingBranchInst--;
-      // exitingBranchInst--;
-
-      // Instruction *I = &(*exitingBranchInst);
-      // I->print(errs());
-      // auto module = I->getFunction()->getParent();
-
-      // auto newExitingBlock = SplitBlock(exitingBlock, I, DT, LI);
-      // auto threadBlock = SplitBlock(exitingBlock, &(*exitingBlock->begin()), DT, LI);
-      // auto detachBlock = exitingBlock;
-
-      // exitBlock = L->getUniqueExitBlock();
-      // if (exitBlock == nullptr) {
-      //   return false;
-      // }
-      // auto newExitBlock = SplitBlock(exitBlock, &(*exitBlock->begin()), DT, LI);
-
-      // auto preHeader = L->getLoopPreheader();
-      // IRBuilder<> TmpB(preHeader, preHeader->begin());
-      // auto SyncRegion = TmpB.CreateCall(
-      //   Intrinsic::getDeclaration(module, Intrinsic::syncregion_start), {});
-
-      // // Create the detach.
-      // IRBuilder<> Builder(detachBlock->getTerminator());
-      // DetachInst *DetachI = Builder.CreateDetach(threadBlock, newExitingBlock, dyn_cast<Instruction>(SyncRegion));
-      // detachBlock->getTerminator()->eraseFromParent();
-      // // Create the reattach.
-      // Builder.SetInsertPoint(threadBlock->getTerminator());
-      // ReattachInst *ReattachI = Builder.CreateReattach(newExitingBlock, SyncRegion);
-      // threadBlock->getTerminator()->eraseFromParent();
-      // // Create the sync.
-      // Builder.SetInsertPoint(exitBlock->getTerminator());
-      // SyncInst *SyncI = Builder.CreateSync(newExitBlock, SyncRegion);
-      // exitBlock->getTerminator()->eraseFromParent();
-
-      // // Sync infrequent pass
-      // BasicBlock* header = preHeader->getSingleSuccessor();
-      // BranchInst* headerBR = dyn_cast<BranchInst>(header->getTerminator());
-      // headerBR->print(errs());
-      // BasicBlock* IFPBB = headerBR->getSuccessor(0) == detachBlock ?
-      //   headerBR->getSuccessor(1) : headerBR->getSuccessor(0);
-      // BasicBlock* IFPBBOld =  SplitBlock(IFPBB, &*IFPBB->begin(), DT, LI);
-
-      // Builder.SetInsertPoint(IFPBB->getTerminator());
-      // SyncInst *SyncIFP = Builder.CreateSync(IFPBBOld, SyncRegion);
-      // IFPBB->getTerminator()->eraseFromParent();
-
-      // errs() << "### end\n";
-      // return true;
     }
 
     void getAnalysisUsage(AnalysisUsage &AU) const override {
